73d5247edd0fe65c69aa5214e7573aded296dbbb
DESKTOP-UNQT1GS\DH <d.hirt@indivikar.ch>
2026-01-06 15:17:37 +0100

    Scritte pro mm geändert

--- IoT/sketche/SchneidMaschine/SchneidMaschine.ino
+++ IoT/sketche/SchneidMaschine/SchneidMaschine.ino
@@ -1,6 +1,6 @@
 // Config
 //double mmInSteps = 12.7;         // wieviel Steps sind ein mm
-double mmInSteps = 13.1;         // wieviel Steps sind ein mm
+double mmInSteps = 13.3;         // wieviel Steps sind ein mm
 
 int startDelay = 5000;          // Start-Pause zwischen den Steps für langsamen Anlauf vom SchrittMotor
 int minDelay = 500;             // min-Pause zwischen den Steps, beeinflusst die Drehzahl, darf nicht < 200 sein
